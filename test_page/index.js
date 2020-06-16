2e4443fb55a245298dd29571a7701196abacb874
hungdvu1710 <hungvsme@gmail.com>
2020-06-16 16:57:04 +0700

    1st commit on test-page

--- test_page/index.js
+++ test_page/index.js
@@ -1,6 +1,36 @@
 const { ipcRenderer } = require("electron");
+const questionTable = document.querySelector(".all-questions")
 
 ipcRenderer.on('excercise-to-load',(event,args)=>{
   console.log("hi")
-  console.log(args)
+  const {questions} = args
+  questions.forEach((element)=>{
+    const{question, type, answer, rightanswer} = element
+    if(type === "single-choice"){
+      addSingleChoiceQuestion(question,rightanswer,answer)
+    } else if(type === "T/F"){
+      addTFQuestion(question,rightanswer,answer)
+    } else{
+      addMultiChoiceQuestion(question,rightanswer,answer)
+    }
+  })
 })
+
+function addSingleChoiceQuestion(question,rightanswer,answer){
+  console.log("single")
+  const questionRow = questionTable.insertRow(-1)
+  questionRow.innerHTML =`
+    <td>Question</td>
+    <td>${question}</td>
+  `
+  const answerRow = questionTable.insertRow(-1)
+  console.log(answer)
+}
+
+function addMultiChoiceQuestion(question,rightanswer,answer){
+  console.log("multi")
+}
+
+function addTFQuestion(question,rightanswer,answer){
+  console.log("T/F")
+}
