8c5853b4e4323abf541f7d4ab33dc82535a286ed
hungdvu1710 <hungvsme@gmail.com>
2020-06-17 16:40:49 +0700

    add sending username between browser windows
    Send username from login -> excercise-selector, excercise-selector-> main

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ ipcMain.on('log-in-req', (event, args) =>{
     }
 
     if (password == doc.password) {
-      openExcerciseSelectorWindow()
+      openExcerciseSelectorWindow(username)
       event.sender.send('close-login-page')
     }
   })
@@ -116,7 +116,7 @@ ipcMain.on('sign-up-req', (event,args) =>{
       return
     } else {
       credentialDb.insert(args)
-      openExcerciseSelectorWindow()
+      openExcerciseSelectorWindow(username)
       event.sender.send('close-login-page')
     }
   })
@@ -137,7 +137,8 @@ ipcMain.on('get-all-excercises',(event,args)=>{
 })
 
 ipcMain.on('req-single-excercise',(event,args)=>{
-  excerciseDb.findOne({name: args}, (err,doc)=>{
+  const {username,excercise} = args
+  excerciseDb.findOne({name: excercise}, (err,doc)=>{
     openTestPageWindow(doc)
     event.sender.send('close-excercise-selector-page')
   })
@@ -186,7 +187,7 @@ ipcMain.on('user-responses',(event,args)=>{
       title: "Score",
       message: `You've earned yourself the score of ${score}/${questions.length}`
     }).then(()=>{
-      openExcerciseSelectorWindow()
+      openExcerciseSelectorWindow("Hoho") //hard fixed
       event.sender.send('close-test-page')
     }).catch(console.error)
     
